59a8df17ba28b2573da8f3ae0b9d84859b17668e
YouseDL <youcef.h2014@gmail.com>
2023-11-07 19:39:13 +0100

    fix product item responsive

--- resources/js/components/Products/Product/product.tsx
+++ resources/js/components/Products/Product/product.tsx
@@ -4,25 +4,25 @@ import { StarFilledIcon, StarIcon } from "@radix-ui/react-icons";
 const Product = () => {
     return (
         <>
-            <div className="w-full flex flex-col items-center justify-center border-2 overflow-hidden cursor-pointer">
-                <div className="w-full h-72  relative bg-cover bg-center"
+            <div className="w-full flex flex-col items-center justify-center border-2 overflow-hidden cursor-pointer rounded-md shadow-md">
+                <div className="w-full h-36 md:h-52  relative bg-cover bg-center"
                     style={{ backgroundImage: "url(https://odour-demo.myshopify.com/cdn/shop/products/product_15_6ee97ac4-83ee-4d04-9b4f-dd32b1c4175a_large.png?v=1561608533)" }}
                 >
                 </div>
                 <div className="w-full flex flex-col items-center justify-center gap-3 py-3">
-                    <p className="text-gray-600 text-center text-sm md:text-lg">
+                    <p className="text-gray-600 text-center text-xs md:text-base lg:tex">
                         Product Name
                     </p>
-                    <p className="text-rose-600 text-center font-bold text-sm md:text-lg">
+                    <p className="text-rose-600 text-center font-bold text-xs md:text-base lg:text-lg">
                         100.00 DZD/G
                     </p>
                     {/* ADD REVIEW STARS */}
                     <div className="flex items-center justify-center">
-                        <StarFilledIcon className="w-6 h-6 text-yellow-500" />
-                        <StarFilledIcon className="w-6 h-6 text-yellow-500" />
-                        <StarFilledIcon className="w-6 h-6 text-yellow-500" />
-                        <StarFilledIcon className="w-6 h-6 text-yellow-500" />
-                        <StarIcon className="w-6 h-6 text-yellow-500" />
+                        <StarFilledIcon className="w-4 h-4 md:w-5 md:h-5 text-yellow-500" />
+                        <StarFilledIcon className="w-4 h-4 md:w-5 md:h-5 text-yellow-500" />
+                        <StarFilledIcon className="w-4 h-4 md:w-5 md:h-5 text-yellow-500" />
+                        <StarFilledIcon className="w-4 h-4 md:w-5 md:h-5 text-yellow-500" />
+                        <StarIcon className="w-4 h-4 md:w-5 md:h-5 text-yellow-500" />
                     </div>
 
                 </div>
--- resources/js/components/Products/products.tsx
+++ resources/js/components/Products/products.tsx
@@ -3,7 +3,7 @@ import Product from "./Product/product";
 const Products = () => {
     return (
         <>
-            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-0 p-7 content-center">
+            <div className="grid grid-cols-2 gap-0 p-7 sm:grid-cols-4 px-0 lg:px-7 xl:px-36 content-center">
                 <div className="flex flex-col items-center justify-center p-2">
                     <Product />
                 </div>
