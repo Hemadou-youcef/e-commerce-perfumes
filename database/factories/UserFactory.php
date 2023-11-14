c8897b6aa33d94c2f11e6101c80b0e71dbd37fab
SAM <samibld18@gmail.com>
2023-11-14 21:07:05 +0100

    add user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,11 +21,17 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'username' => $this->faker->unique()->userName,
+            'phone' => $this->faker->unique()->phoneNumber,
+            'address' => $this->faker->address,
+            'role' => 'guest',
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'password' => bcrypt('password'), // Replace 'password' with your desired default password
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
