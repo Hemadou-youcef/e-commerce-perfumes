073e25bab04c624dfa170fa7c73f6a3a38357b1b
Sambld <samibld18@gmail.com>
2023-12-04 23:01:59 +0100

    add chart data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,24 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Inertia\Inertia;
 use Inertia\Response;
+use function auth;
+use function request;
 
 class DashboardController extends Controller
 {
-
-
 
 
     public function index(): Response
     {
 
         $data = [];
-        if (\auth()->user()->isAdmin()) {
+        if (auth()->user()->isAdmin()) {
             $data = $this->adminData();
-        } elseif (\auth()->user()->isEmployee()) {
+        } elseif (auth()->user()->isEmployee()) {
             $data = $this->employeeData();
         }
 
@@ -29,21 +28,21 @@ class DashboardController extends Controller
         ]);
 
 
-
-
-
     }
 
 
-
-    private function sales($startDate = null , $endDate = null): int
+    public function sales($startDate = null, $endDate = null): int
     {
-        if(!$startDate) {   $startDate = now()->subDay(); }
+        if (!$startDate) {
+            $startDate = now()->subDay();
+        }
 
-        if(!$endDate) {    $endDate = now();   }
+        if (!$endDate) {
+            $endDate = now();
+        }
 
         return Order::query()
-            ->where('status', ['confirmed' , 'delivered'])
+            ->whereIn('status', ['confirmed', 'delivered'])
             ->whereBetween('created_at', [
                 $startDate,
                 $endDate,
@@ -53,14 +52,20 @@ class DashboardController extends Controller
     }
 
 
-    public function profit($startDate = null , $endDate = null): int
-    {
-        if(!$startDate) {   $startDate = now()->subMonth(); }
+    // order counts function and make the default startDate now and endDate 1 month ago
 
-        if(!$endDate) {    $endDate = now();   }
+    public function profit($startDate = null, $endDate = null): int
+    {
+        if (!$startDate) {
+            $startDate = now()->subMonth();
+        }
+
+        if (!$endDate) {
+            $endDate = now();
+        }
 
         return Order::query()
-            ->where('status', ['confirmed' , 'delivered'])
+            ->whereIn('status', ['confirmed', 'delivered'])
             ->whereBetween('created_at', [
                 $startDate,
                 $endDate,
@@ -69,15 +74,14 @@ class DashboardController extends Controller
 
     }
 
-
-
-
-
-    // order counts function and make the default startDate now and endDate 1 month ago
-    private function ordersCount($startDate = null, $endDate = null): array
+    public function ordersCount($startDate = null, $endDate = null): array
     {
-        if(!$startDate) {   $startDate = now()->subMonth(); }
-        if(!$endDate) {    $endDate = now();   }
+        if (!$startDate) {
+            $startDate = now()->subMonth();
+        }
+        if (!$endDate) {
+            $endDate = now();
+        }
 
         $orders = Order::query()
             ->whereBetween('created_at', [$startDate, $endDate])
@@ -92,82 +96,202 @@ class DashboardController extends Controller
         ];
     }
 
-
-    private function orders($startDate = null, $endDate = null , $status = null , $limit = 5 )
+    public function orders($startDate = null, $endDate = null, $status = null, $limit = 5)
     {
-        if(!$startDate) {   $startDate = now()->subMonth(); }
+        if (!$startDate) {
+            $startDate = now()->subMonth();
+        }
 
-        if(!$endDate) {    $endDate = now();   }
+        if (!$endDate) {
+            $endDate = now();
+        }
 
-        $orders = Order::query()
+        if (!$status) {
+            $status = 'delivered';
+        }
+
+        return Order::query()
             ->whereBetween('created_at', [$startDate, $endDate])
-            ->when($status == 'completed' , fn($query , $status) => $query->where('status' , ['confirmed' , 'delivered']))
-            ->when($status , fn($query , $status) => $query->where('status' , $status))
+            ->where('status', $status)
             ->take($limit)
             ->get()
             ->map(fn(Order $order) => [
                 'id' => $order->id,
                 'user' => $order->user->name,
+                'profit' => $order->profit,
                 'total' => $order->total,
                 'status' => $order->status,
                 'created_at' => $order->created_at->format('Y-m-d H:i'),
             ]);
-
-        ;
-
-        return $orders;
     }
 
-
-    private function adminData(): array
+    public function adminData(): array
     {
 
-        $salesStartDate = \request('salesStartDate');
-        $salesEndDate = \request('salesEndDate');
+        $salesStartDate = request('salesStartDate');
+        $salesEndDate = request('salesEndDate');
 
-        $profitStartDate = \request('profitStartDate');
-        $profitEndDate = \request('profitEndDate');
+        $profitStartDate = request('profitStartDate');
+        $profitEndDate = request('profitEndDate');
 
-        $ordersCountStartDate = \request('ordersCountStartDate');
-        $ordersCountEndDate = \request('ordersCountEndDate');
+        $ordersCountStartDate = request('ordersCountStartDate');
+        $ordersCountEndDate = request('ordersCountEndDate');
 
-        $ordersStatus = \request('ordersStatus');
-        $ordersLimit = \request('ordersLimit');
-        $orderStartDate = \request('orderStartDate');
-        $orderEndDate = \request('orderEndDate');
+        $ordersStatus = request('ordersStatus');
+        $ordersLimit = request('ordersLimit');
+        $orderStartDate = request('orderStartDate');
+        $orderEndDate = request('orderEndDate');
 
-        $adminData = [
+
+        $chartStartDate = request('chartStartDate');
+        $chartEndDate = request('chartEndDate');
+        $chartPeriod = request('chartPeriod'); // yearly, monthly, daily
+
+        return [
             'sales' => $this->sales($salesStartDate, $salesEndDate),
             'profit' => $this->profit($profitStartDate, $profitEndDate),
             'ordersCount' => $this->ordersCount($ordersCountStartDate, $ordersCountEndDate),
-            'orders' => $this->orders($orderStartDate,$orderEndDate, $ordersStatus, $ordersLimit),
-        ];
+            'orders' => $this->orders($orderStartDate, $orderEndDate, $ordersStatus, $ordersLimit),
+            'clientsCount' => User::query()
+                ->whereHas('orders', fn($query) => $query->whereIn('status', ['confirmed', 'delivered']))
+                ->count(),
+            'profitChart' => $this->profitChart( $chartStartDate, $chartEndDate, $chartPeriod),
 
-        return $adminData;
+        ];
     }
 
-
-    private function employeeData(): array
+    public function employeeData(): array
     {
 
-        $ordersCountStartDate = \request('ordersCountStartDate');
-        $ordersCountEndDate = \request('ordersCountEndDate');
+        $ordersCountStartDate = request('ordersCountStartDate');
+        $ordersCountEndDate = request('ordersCountEndDate');
 
         $ordersStatus = 'confirmed';
-        $ordersLimit = \request('ordersLimit');
-        $orderStartDate = \request('orderStartDate');
-        $orderEndDate = \request('orderEndDate');
+        $ordersLimit = request('ordersLimit');
+        $orderStartDate = request('orderStartDate');
+        $orderEndDate = request('orderEndDate');
 
+        $chartStartDate = request('chartStartDate');
+        $chartEndDate = request('chartEndDate');
+        $chartPeriod = request('chartPeriod'); // yearly, monthly, daily
         return [
             'ordersCount' => $this->ordersCount($ordersCountStartDate, $ordersCountEndDate),
-            'orders' => $this->orders($orderStartDate,$orderEndDate, $ordersStatus, $ordersLimit),
+            'orders' => $this->orders($orderStartDate, $orderEndDate, $ordersStatus, $ordersLimit),
+            'ordersCountChart' => $this->ordersCountChart( $chartStartDate, $chartEndDate, $chartPeriod),
+
+
         ];
 
     }
 
 
+    public function profitChart(
+        $startDate = null,
+        $endDate = null,
+        $period = 'monthly'
+    ): array
+    {
 
+        if (!$startDate) {
+            $startDate = now()->subYear();
+        }
 
+        if (!$endDate) {
+            $endDate = now();
+        }
+
+        switch ($period) {
+            case "yearly":
+                $format = 'Y';
+                $interval = '1 year'; // Modify intervals to ensure they are strings
+                break;
+
+            case "monthly":
+                $format = 'F Y';
+                $interval = '1 month';
+                break;
+
+            case "daily":
+                $format = 'm/d/Y';
+                $interval = '1 day';
+                break;
+
+            default:
+                $format = 'F Y';
+                $interval = '1 month';
+        }
+
+        $orders = Order::query()
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->get();
+
+        $profit = [];
+
+        for ($date = clone $startDate; $date->lte($endDate); $date->add($interval)) {
+            $periodLabel = $date->format($format);
+
+            $periodProfit = $orders->filter(function ($order) use ($periodLabel, $format) {
+                return $order->created_at->format($format) == $periodLabel;
+            })->sum(function ($order) {
+                return $order->profit;
+            });
+
+            $profit[$periodLabel] = $periodProfit;
+        }
+
+        return $profit;
+    }
+
+    public function ordersCountChart( $startDate = null, $endDate = null, $period = 'monthly'): array
+    {
+        if (!$startDate) {
+            $startDate = now()->subYear();
+        }
+
+        if (!$endDate) {
+            $endDate = now();
+        }
+
+        switch ($period) {
+            case "yearly":
+                $format = 'Y';
+                $interval = '1 year'; // Modify intervals to ensure they are strings
+                break;
+
+            case "monthly":
+                $format = 'F Y';
+                $interval = '1 month';
+                break;
+
+            case "daily":
+                $format = 'm/d/Y';
+                $interval = '1 day';
+                break;
+
+            default:
+                $format = 'F Y';
+                $interval = '1 month';
+        }
+
+        $orders = Order::query()
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->where('status', 'delivered')
+            ->get();
+
+        $ordersCount = [];
+
+        for ($date = clone $startDate; $date->lte($endDate); $date->add($interval)) {
+            $periodLabel = $date->format($format);
+
+            $periodOrdersCount = $orders->filter(function ($order) use ($periodLabel, $format) {
+                return $order->created_at->format($format) == $periodLabel;
+            })->count();
+
+            $ordersCount[$periodLabel] = $periodOrdersCount;
+        }
+
+        return $ordersCount;
+    }
 
 
 
