2b29bb32cfb462714a1ceaedd5267b17844bb909
Sambld <samibld18@gmail.com>
2023-12-27 18:50:52 +0100

    fix add product type

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -27,7 +27,7 @@ class UpdateProductRequest extends FormRequest
             'description_ar' => 'sometimes|required|string',
             'status' => 'nullable|string',
             'image' => 'sometimes|required|image|mimes:jpeg,png,jpg|max:8192',
-            'type' => 'sometimes|required|integer|in:1,2',
+            'type' => 'sometimes|required|integer|in:1,2,3',
             'category_ids.*' => 'sometimes|exists:categories,id',
             'prices' => [
                 'sometimes',
